6d5346e16d07157cae58f6453889ca79438bd522
Jony <jonyreiscardoso@gmail.com>
2020-06-14 12:06:36 -0300

    Updates

---
+++ precache-manifest.ac4b2a3cb1ba4d4717d86e0c944a08d8.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9db063b3d16f943a62937eaf4eb0fc",
+    "url": "/surfbot/index.html"
+  },
+  {
+    "revision": "a3275f9a2673a7b13d51",
+    "url": "/surfbot/static/js/2.d73e0dc6.chunk.js"
+  },
+  {
+    "revision": "b92b369c485a56d25a5a7495ecf46b59",
+    "url": "/surfbot/static/js/2.d73e0dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaf877af855f094b2ccf",
+    "url": "/surfbot/static/js/main.d2fb0b96.chunk.js"
+  },
+  {
+    "revision": "69cad23e7ef42eaf590b",
+    "url": "/surfbot/static/js/runtime-main.87784e56.js"
+  },
+  {
+    "revision": "6a0876d299e1c69eca32d24e0dfa391e",
+    "url": "/surfbot/static/media/Vector.6a0876d2.svg"
+  },
+  {
+    "revision": "b76c17a300c4371ddee96bdd84c445d4",
+    "url": "/surfbot/static/media/bg-intro.b76c17a3.png"
+  },
+  {
+    "revision": "b082689bdcdc73db007961357137eb83",
+    "url": "/surfbot/static/media/bg-onda.b082689b.png"
+  },
+  {
+    "revision": "253c7599f4f25bd0fea827c18e3936b6",
+    "url": "/surfbot/static/media/boia.253c7599.svg"
+  },
+  {
+    "revision": "ed399be3e4877c8c6d45b39f188d769a",
+    "url": "/surfbot/static/media/coco.ed399be3.svg"
+  },
+  {
+    "revision": "1edc0acc4029998ef66e7fb06ed967da",
+    "url": "/surfbot/static/media/foto-local.1edc0acc.png"
+  },
+  {
+    "revision": "83e86579b2443b0c833cfbfd78eb1bc1",
+    "url": "/surfbot/static/media/foto-mapa.83e86579.png"
+  },
+  {
+    "revision": "0c70644144ef554e24a441dc295615ef",
+    "url": "/surfbot/static/media/logo-surfbot.0c706441.svg"
+  },
+  {
+    "revision": "4950fe7197edc2d61cc87acb9fe27109",
+    "url": "/surfbot/static/media/prancha.4950fe71.svg"
+  },
+  {
+    "revision": "b8f41594ac23a48b9256b7179c7b9757",
+    "url": "/surfbot/static/media/sandalhas.b8f41594.svg"
+  }
+]);
